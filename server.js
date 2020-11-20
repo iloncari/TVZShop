b5add658f48dfeff367341731b25aebd96ded896
iloncari <44259324+iloncari@users.noreply.github.com>
2020-11-20 15:54:36 +0100

    TVZShop login/register and home

--- server.js
+++ server.js
@@ -6,19 +6,16 @@ let path = require('path');
 let jwt = require('jsonwebtoken');
 let bcrypt = require('bcrypt-nodejs');
 let mongo=require("mongodb").MongoClient;
-
 let SerpWow = require('google-search-results-serpwow')
-
 
 let config = require('./config');
 
 mongo.connect(config.pool, function(err, database){
-    let path = require('path')
-    if (err) { console.error('Problem connecting to database');  } else {
-
+    if (err) {
+        console.error('Problem connecting to database');
+    } else {
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
-
 
         app.use(express.static(__dirname+'/public/app'));
         app.use(express.static(path.join(__dirname, 'public')));
@@ -30,27 +27,19 @@ mongo.connect(config.pool, function(err, database){
         });
         app.use(morgan('dev'));
 
-        let authRouter = require('./app/routes/authenticate')(app,express,database.db('NJPOglasnikAppLoncaric'),jwt,config.secret, bcrypt);
+        let authRouter = require('./app/routes/authenticate')(app,express,database.db('TVZShop'),jwt,config.secret, bcrypt);
         app.use('/authenticate', authRouter);
 
-        let apiRouter = require('./app/routes/api')(app,express,database.db('NJPOglasnikAppLoncaric'), jwt, config.secret, SerpWow);
+        let apiRouter = require('./app/routes/api')(app,express,database.db('TVZShop'), jwt, config.secret, SerpWow);
         app.use('/api', apiRouter);
-
-
-
 
         app.get('*', function(req, res) {
             res.sendFile(path.join(__dirname + '/public/app/index.html'));
         });
 
-
-
-
-
         app.listen(config.port);
 
         console.log('Running on port ' + config.port);
-
     }
 
 });
