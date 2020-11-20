debe17a173980e30571b1c1d14a4e45dce3b46c8
iloncari <44259324+iloncari@users.noreply.github.com>
2020-11-20 13:38:35 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let path = require('path');
+let jwt = require('jsonwebtoken');
+let bcrypt = require('bcrypt-nodejs');
+let mongo=require("mongodb").MongoClient;
+
+let SerpWow = require('google-search-results-serpwow')
+
+
+let config = require('./config');
+
+mongo.connect(config.pool, function(err, database){
+    let path = require('path')
+    if (err) { console.error('Problem connecting to database');  } else {
+
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+
+
+        app.use(express.static(__dirname+'/public/app'));
+        app.use(express.static(path.join(__dirname, 'public')));
+        app.use(function(req, res, next) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
+            next();
+        });
+        app.use(morgan('dev'));
+
+        let authRouter = require('./app/routes/authenticate')(app,express,database.db('NJPOglasnikAppLoncaric'),jwt,config.secret, bcrypt);
+        app.use('/authenticate', authRouter);
+
+        let apiRouter = require('./app/routes/api')(app,express,database.db('NJPOglasnikAppLoncaric'), jwt, config.secret, SerpWow);
+        app.use('/api', apiRouter);
+
+
+
+
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname + '/public/app/index.html'));
+        });
+
+
+
+
+
+        app.listen(config.port);
+
+        console.log('Running on port ' + config.port);
+
+    }
+
+});
+
+
+
+
+
